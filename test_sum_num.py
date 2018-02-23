18860c1d19f9a246e89de605d6b03dff2f5e2b43
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-02-22 21:51:31 -0500

    update test_sum_num to work with object oriented code

--- test_sum_num.py
+++ test_sum_num.py
@@ -1,16 +1,24 @@
 def test_sum_numbers():
-    from sum_num import sum_numbers
+    from ListOperations_OOP import ListOperations
     import pytest
     import math
-    summed_output_1 = sum_numbers([1, 5, 8, 3, 6, 3])
-    summed_output_2 = sum_numbers([-4, 5, 7, 5, 3, -5])
-    summed_output_3 = sum_numbers([0.5, -0.3, 0.2, -0.5])
-    assert summed_output_1 == 26
-    assert summed_output_2 == 11
-    assert summed_output_3 - 0.1 < 0.0001
+
+    list1 = ListOperations([1, 5, 8, 3, 6, 3])
+    list2 = ListOperations([-4, 5, 7, 5, 3, -5])
+    list3 = ListOperations([0.5, -0.3, 0.2, -0.5])
+
+    list1.sum_numbers()
+    list2.sum_numbers()
+    list3.sum_numbers()
+    assert list1.sum_num == 26
+    assert list2.sum_num == 11
+    assert list3.sum_num - 0.1 < 0.0001
+
     with pytest.raises(ValueError):
-        sum_numbers(math.sqrt(-2), math.sqrt(4), math.sqrt(2))
+        list4 = ListOperations([math.sqrt(-2),math.sqrt(4),math.sqrt(2)])
+        list4.sum_numbers()
     with pytest.raises(TypeError):
-        sum_numbers("1", 2, 3)
+        list5 = ListOperations(["1", 2, 3])
+        list5.sum_numbers()
     with pytest.raises(ImportError):
         import MyLittlePony
